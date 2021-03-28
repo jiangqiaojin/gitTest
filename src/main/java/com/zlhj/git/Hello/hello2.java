51bdd3d7e7e028b6664c56491335fa318604ff8c
JQJ <1499718133@qq.com>
2021-03-28 14:20:40 +0800

    改动方法

--- src/main/java/com/zlhj/git/Hello/hello2.java
+++ src/main/java/com/zlhj/git/Hello/hello2.java
@@ -6,5 +6,7 @@ package com.zlhj.git.Hello;
 public class hello2 {
     public void test(){
         System.out.println("这里是方法");
+        System.out.println("改动方法");
+        System.out.println();
     }
 }
