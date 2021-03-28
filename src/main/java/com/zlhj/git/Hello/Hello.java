7e35c991344e0f4b628bdb28f5fcd88462a05444
JQJ <1499718133@qq.com>
2021-03-28 14:05:17 +0800

    git Test

--- src/main/java/com/zlhj/git/Hello/Hello.java
+++ src/main/java/com/zlhj/git/Hello/Hello.java
@@ -3,7 +3,7 @@ package com.zlhj.git.Hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello gti");
-
-        System.out.println();
+        System.out.println("啊这");
+        System.out.println("我就在这里测试");
     }
 }
